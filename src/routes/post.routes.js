22e409bad4dfda2cf913cc629e9fd611dec19576
ritam-g <ritammaty2006@gmail.com>
2026-02-13 16:00:29 +0530

    fix: correct comments for multer storage and post route handling

--- src/routes/post.routes.js
+++ src/routes/post.routes.js
@@ -3,10 +3,10 @@ const postController = require("../controllers/post.controller");
 const multer  = require('multer')
 
 const postRoute=express.Router()
-const storage = multer.memoryStorage()
+const storage = multer.memoryStorage() //! this is for memeory storege 
 const upload = multer({ storage: storage })
 
 
-postRoute.post('/',upload.single('image'),postController)
+postRoute.post('/',upload.single('image'),postController)//! now express can red the form-data file  
 
 module.exports=postRoute
